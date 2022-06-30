3eb112c919aa55c6d98d42c4aa570d913875ad64
fiigmnt <fiigmnt@protonmail.com>
2022-06-30 14:27:06 -0400

    init

---
+++ pages/api/meta/birth/[tokenId].ts
@@ -0,0 +1,42 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type TokenMeta = {
+  name: string;
+  description: string;
+  external_url: string;
+  image: string;
+  audio_url: string;
+};
+
+// TODO: move this to a general util file
+const validate = (query: any) => {
+  if (typeof query === 'object') {
+    if (!query.tokenId) {
+      throw new Error('tokenId not provided');
+    }
+    return {
+      tokenId: query.tokenId,
+    };
+  }
+  throw new Error('invalid query');
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<TokenMeta>
+) {
+  const { tokenId } = validate(req.query);
+
+  const editionDigit = ('0' + tokenId).slice(-2);
+
+  const tokenMeta = {
+    name: `BIRTH ${editionDigit}`,
+    description: 'BIRTH is a 2022 release by PRINCESS BLEACH.',
+    external_url: 'https://princessbleach.com/',
+    image: `https://princessbleach.com/drops/birth/${tokenId}.png`,
+    audio_url: 'https://princessbleach.com/drops/birth.mp3',
+  };
+
+  res.status(200).json(tokenMeta);
+}
---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>PRINCESS BLEACH</title>
+        <meta name="description" content="PRINCESS BLEACH" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          PRINCESS BLEACH
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
