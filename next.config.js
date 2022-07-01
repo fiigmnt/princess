431af5ef9e639c98dba85347cba6bd8780d1aecb
fiigmnt <fiigmnt@protonmail.com>
2022-07-01 01:53:56 -0400

    tester

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  rewrites: async () => [
+    {source: '/api/tester', destination: '/api/meta/birth/1'},
+  ],
   reactStrictMode: true,
 }
 
