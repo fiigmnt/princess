4733b08024879ca03461b048b17f89b3680a408f
fiigmnt <fiigmnt@protonmail.com>
2022-07-01 15:27:33 -0400

    testing

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   // rewrites: async () => [
   //   {source: '/api/tester', destination: '/api/meta/birth/1'},
   // ],
+  async redirects() {
+    return [
+      {
+        source: '/m/*',
+        destination: 'https://c6qoiw9d4k.execute-api.us-east-1.amazonaws.com/*',
+        permanent: true,
+      },
+    ]
+  },
   reactStrictMode: true,
 }
 
